0c642d5a3c744301240ccdce203e7bcc1f3d753b
Shagon1k (Aliaksei Hurynovich) <shagon1k@gmail.com>
2024-05-12 12:56:55 +0300

    [Task-10] Add healthcheck to the app

--- task-10/src/server.ts
+++ task-10/src/server.ts
@@ -8,6 +8,8 @@ import {
 } from "@mikro-orm/core";
 import { type PostgreSqlDriver as IPostgreSqlDriver } from "@mikro-orm/postgresql";
 import apiRouter from "./api";
+import generateResponse from "./api/helpers/generateResponse";
+import { STATUS_CODES } from "./constants";
 import { APP_ENV } from "./config/app.config";
 import ormConfig from "./config/orm.config";
 
@@ -24,6 +26,33 @@ export const runServer = async () => {
   // Note: Create new Identity Map for each request to avoid having one context for whole application
   app.use((req, res, next) => RequestContext.create(orm.em, next));
   app.use("/api", apiRouter);
+  app.get("/health", async (_, res, next) => {
+    try {
+      const isDBConnected = await orm.isConnected();
+      if (!isDBConnected) {
+        return res
+          .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+          .json(
+            generateResponse(
+              undefined,
+              "Unhealthy. Error connecting to database."
+            )
+          );
+      }
+
+      res
+        .status(STATUS_CODES.OK)
+        .json(generateResponse({ message: "Healthy." }));
+    } catch (error) {
+      console.error("Internal error performing healthcheck:", error);
+
+      return res
+        .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .send(generateResponse(undefined, "Unhealthy."));
+    }
+
+    next();
+  });
 
   app.listen(process.env.APP_PORT, () => {
     console.log(`Server is running on port ${process.env.APP_PORT}`);
